80f394a0682893695bed9efbe59dd24d3fcb422d
mibeeb <averkievavera1@gmail.com>
2024-12-01 13:04:58 +0300

    init: create personnel operation

--- app/src/modules/personnel-operation/table-personnel-operations.vue
+++ app/src/modules/personnel-operation/table-personnel-operations.vue
@@ -26,45 +26,86 @@
           </tr>
         </tbody>
       </table>
-    </div>
-    <div class="button-container">
+  </div>
+  <div class="button-container">
     <div class="button">
-      <button>Добавить запись</button>
+      <button @click="showFormCreateOper = !showFormCreateOper">
+        {{ showFormCreateOper ? "Скрыть форму добавления" : "Добавить запись" }}
+      </button>
     </div>
-    <div class="button">
-      <button>Удалить запись</button>
-    </div>
-    <div class="button">
-      <button>Поиск записи</button>
-    </div>
-    <div class="button">
-      <button>Обновить таблицу</button>
-    </div>
+      <div class="button">
+        <button @click="deleteOperation">Удалить запись</button>
+      </div>
+      <div class="button">
+        <button @click="getOneOperation">Поиск записи</button>
+      </div>
+      <div class="button">
+        <button @click="updateOperation">Обновить таблицу</button>
+      </div>
     </div>
   </div>
+  <CreatePersonnelOperations v-if="showFormCreateOper" />
 </template>
 <script>
 import axios from "axios";
+import CreatePersonnelOperations from "./create-personnel-operations.vue";
 export default {
-    name: "TablePersonnelOperation",
-    data(){
-        return {
-            items: [],
-        };
+  name: "TablePersonnelOperation",
+  components: {
+    CreatePersonnelOperations,
+  },
+  data() {
+    return {
+      showFormCreateOper: false,
+      items: [],
+    };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get(`http://localhost:8081/PerOP`);
+        this.items = response.data;
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
     },
-    mountad() {
-        this.fetshData();
-    },
-    methods: {
-        async fetchData() {
-            try{
-                const response = await axios.get(`http://localhost:8081/PerOp`);
-                this.items = response.data;
-            } catch (error) {
-                console.error ("Error fetching data: ", error);
+    async deleteOperation() {
+      const operationId = prompt("Введите ID для удаления: ");
+      if (operationId) {
+        try {
+          await axios.get(
+            "http://localhost:8081/PerOP/personnel_operations/:id",{
+              operationId
             }
+          );
+          this.fetchData();
+        } catch (error) {
+          console.error("Error deleting operation: ", error);
         }
-    }
-}
+      }
+    },
+    async getOneOperation() {
+      const getOneOperation = prompt(" Введите название для поиска : ");
+      if (getOneOperation) {
+        try {
+          const response = await axios.get(
+            "http://localhost:8081/PerOP/personnel_operations/:id",{
+              getOneOperation
+            }
+          );
+          this.items = response.data.operations || [];
+        } catch (error) {
+          console.error("Error searching for operation: ", error);
+        }
+      }
+    },
+    async updateOperation() {
+      this.fetchData();
+    },
+  },
+};
 </script>
 <style src="../../assets/style.css"></style>
